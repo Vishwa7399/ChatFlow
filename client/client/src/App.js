180bca7f8286fed5b7d39d11f9de55199cf93128
Vishwa7399 <vishwaprajapati7337@gmail.com>
2026-06-09 09:41:44 +0530

    Fix client folder tracking

---
+++ client/client/src/App.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import Chat from "./Chat";
+import './App.css';
+import io from 'socket.io-client'
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  }
+
+  const handleEnter = (event) => {
+    if (event.key === "Enter") {
+      joinRoom();
+    }
+  };
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="Vishwa.."
+            onChange={(event) => setUsername(event.target.value)}
+            onKeyDown={handleEnter}
+          />
+
+          <input
+            type="text"
+            placeholder="Room ID.."
+            onChange={(event) => setRoom(event.target.value)}
+            onKeyDown={handleEnter}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+
+        </div>)
+        : (
+          <Chat socket={socket} username={username} room={room} />)
+      }
+
+
+
+    </div>
+  )
+}
+export default App;
